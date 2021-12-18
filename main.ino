66d6f67103cdd59f141eddbf1fb56f47a367efa2
RanjeetPajeet <68920468+RanjeetPajeet@users.noreply.github.com>
2021-12-18 16:18:30 -0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -15,10 +15,6 @@ WiFiClient client;
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
-String jsonBuffer;
-
-const String serverPath = "http://api.weatherapi.com/v1/current.json?key=0e8c22cc6cb047ea8dc221200210112&q=52340";
-
 
 void setup()
 {
@@ -53,28 +49,13 @@ void setup()
 void loop()
 {
 
-  String jsonBuffer = httpGETRequest(serverPath.c_str());
-  if ( jsonBuffer == "{}" )
-  {
-    while ( jsonBuffer == "{}" )
-    {
-      String jsonBuffer = httpGETRequest(serverPath.c_str());
-      delay(10000);   // Wait 10 seconds before trying to get new temperature from API
-    }
-  }
-  JSONVar APIresult = JSON.parse(jsonBuffer);
-
-  double x = APIresult["current"]["temp_f"];
-  float tout = (float)x;
-
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
   float tf = ( t * 1.8 ) + 32;
-  float dt = tf - tout;
 
   if ( client.connect(server, 80) )               // "184.106.153.149" or api.thingspeak.com
   {
-    String sendData = apiKey+"&field1="+String(tf)+"&field2="+String(tout)+"&field3="+String(dt)+"&field4="+String(h)+"\r\n\r\n";
+    String sendData = apiKey+"&field1="+String(tf)+"&field3="+String(h)+"\r\n\r\n";
 
     client.print("POST /update HTTP/1.1\n");
     client.print("Host: api.thingspeak.com\n");
@@ -90,8 +71,6 @@ void loop()
   if ( ! isnan(t) )                               // Check if 'is not a number'
   {
     Serial.print("Temp *F = "); Serial.println(tf);
-    Serial.print("Outside Temp *F = "); Serial.println(tout);
-    Serial.print("Delta Temp *F = "); Serial.println(dt);
   } else if ( isnan(t) ) Serial.println("Failed to read temperature!");
   
   if ( ! isnan(h) )                               // Check if 'is not a number'
@@ -101,30 +80,6 @@ void loop()
   } else if ( isnan(h) ) Serial.println("Failed to read humidity!");
 
   Serial.println();
-  delay(60000);                                   // Wait 1 minute
+  delay(300000);                                   // Wait 5 minutes
   
-}
-
-
-
-String httpGETRequest(const char* serverName) 
-{
-  WiFiClient client;
-  HTTPClient http; 
-  http.begin(client, serverName);
-  
-  int httpResponseCode = http.GET();  
-  String payload = "{}"; 
-  
-  if ( httpResponseCode > 0 ) payload = http.getString();
-  
-  else 
-  {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-
-  http.end();
-
-  return payload; 
 }
